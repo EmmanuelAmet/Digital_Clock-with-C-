5c6538a1ab00c0afd4a9296406af05e000e84fd3
EMMANUEL <emmanuelamet11@gmail.com>
2020-04-08 02:38:47 -0700

    Add project files.

---
+++ Digital_Clock/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Digital_Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.sliderphoto = new System.Windows.Forms.PictureBox();
+            this.lbltime = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.sliderphoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(824, 54);
+            this.panel1.TabIndex = 1;
+            // 
+            // sliderphoto
+            // 
+            this.sliderphoto.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sliderphoto.Image = global::Digital_Clock.Properties.Resources.Welcome_Scan;
+            this.sliderphoto.Location = new System.Drawing.Point(0, 54);
+            this.sliderphoto.Name = "sliderphoto";
+            this.sliderphoto.Size = new System.Drawing.Size(824, 519);
+            this.sliderphoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.sliderphoto.TabIndex = 2;
+            this.sliderphoto.TabStop = false;
+            // 
+            // lbltime
+            // 
+            this.lbltime.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbltime.AutoEllipsis = true;
+            this.lbltime.AutoSize = true;
+            this.lbltime.BackColor = System.Drawing.Color.Transparent;
+            this.lbltime.Font = new System.Drawing.Font("Impact", 199.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltime.ForeColor = System.Drawing.Color.Black;
+            this.lbltime.Location = new System.Drawing.Point(7, 126);
+            this.lbltime.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbltime.Name = "lbltime";
+            this.lbltime.Size = new System.Drawing.Size(806, 325);
+            this.lbltime.TabIndex = 23;
+            this.lbltime.Text = "12 : 30";
+            this.lbltime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbltime.Click += new System.EventHandler(this.lbltime_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 573);
+            this.Controls.Add(this.lbltime);
+            this.Controls.Add(this.sliderphoto);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.sliderphoto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox sliderphoto;
+        private System.Windows.Forms.Label lbltime;
+    }
+}
+
---
+++ Digital_Clock/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Digital_Clock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //protected override CreateParams CreateParams
+        //{
+        //    get
+        //    {
+        //        CreateParams cp = base.CreateParams;
+        //        cp.ExStyle |= 0x00000020;
+        //        return cp;// base.CreateParams;
+        //    }
+        //}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //this.BackColor = Color.White;
+            //panel1.BackColor = Color.FromArgb(0, Color.Black);
+            //panel1.BackColor = Color.FromArgb(100, 88, 44, 55);
+
+            //panel1 = new TransparentPanel();
+            //panel1.BackColor = System.Drawing.Color.Transparent;
+            //panel1.Controls.Add(panel1);
+            //panel1.BringToFront();
+        }
+        //protected override void OnPaint(PaintEventArgs e)
+        //{
+        //    //base.OnPaint(e);
+        //    //e.Graphics.DrawLine(Pens.Yellow, 0, 0, 100, 100);
+        //    e.Graphics.FillRectangle(new SolidBrush(this.BackColor), this.ClientRectangle);
+        //}
+
+        private int imageNumber = 1;
+
+        private void LoadNextImage()
+        {
+            if (imageNumber == 31)
+            {
+                imageNumber = 1;
+
+            }
+            sliderphoto.ImageLocation = string.Format(@"images\{0}.jpg", imageNumber);
+            imageNumber++;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            LoadNextImage();
+        }
+
+        private void lbltime_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
